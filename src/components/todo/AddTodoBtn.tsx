7d2e5d7dd2550d79bf362e676270174a1ff02c6a
mhlehri <lehrimirza101@gmail.com>
2024-06-30 20:52:41 +0600

    first commit

---
+++ src/components/todo/AddTodoBtn.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+
+export function AddTodoBtn() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add todo</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add new todo</DialogTitle>
+          <DialogDescription>
+            Fill in the form below to add a new todo.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="title" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="title"
+              defaultValue="Add new feature"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-start gap-4">
+            <Label htmlFor="description" className="text-right">
+              Description
+            </Label>
+            <Textarea
+              id="description"
+              defaultValue="Add a new feature to the app"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ src/redux/features/todoSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+type TTodo = {
+  title: string;
+  description: string;
+  isCompleted: boolean;
+};
+
+type TInitialState = {
+  todos: TTodo[];
+};
+
+const initialState: TInitialState = {
+  todos: [] as TTodo[],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<TTodo>) => {
+      state.todos.push({ ...action.payload, isCompleted: false });
+    },
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
+export default todoSlice.reducer;
